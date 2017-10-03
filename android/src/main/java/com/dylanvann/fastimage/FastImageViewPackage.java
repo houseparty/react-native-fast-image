01c883dad9e74733fa303e1c0138786ad4487b47
Joshua McNeese <jmcneese@gmail.com>
2017-10-03 15:49:43 -0600

    fixing backwards compatibility for RN < 0.47

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
@@ -16,7 +16,9 @@ public class FastImageViewPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Collections.<NativeModule>singletonList(new FastImageViewModule(reactContext));
     }
-
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.<ViewManager>singletonList(new FastImageViewManager());
